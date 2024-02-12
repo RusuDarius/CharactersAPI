b0ac6c42b23b079315f3e6e2f80c483c0610eb2c
Rusu Darius <dariusrusu7@gmail.com>
2024-02-12 23:09:26 +0200

    Initial commit

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PatrickAPI.Controllers
+{
+    [Route("[controller]")]
+    public class SuperHeroController : Controller
+    {
+        private readonly ILogger<SuperHeroController> _logger;
+
+        public SuperHeroController(ILogger<SuperHeroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Entities/SuperHero.cs
@@ -0,0 +1,9 @@
+﻿namespace PatrickAPI;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
